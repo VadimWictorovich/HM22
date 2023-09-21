2dd7d8465af0f940bd180d3e3747b4c2e84a9a78
Vadim <vsadnik199@gmail.com>
2023-09-21 23:54:42 +0300

    Commit #1

---
+++ HW22/HW22/Models/Feedback.swift
@@ -0,0 +1,16 @@
+//
+//  Feedback.swift
+//  HW22
+//
+//  Created by Вадим Игнатенко on 21.09.23.
+//
+
+import Foundation
+
+
+struct Feedback {
+    
+    private let date = Date()
+    private let text: String
+    private let mark: Double    
+}
