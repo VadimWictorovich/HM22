7f6ca54e2744a1dfd61d4b97bd253ed7f0405df3
Vadim <vsadnik199@gmail.com>
2023-09-21 22:18:24 +0300

    Project created

---
+++ HW22/HW22/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW22
+//
+//  Created by Вадим Игнатенко on 21.09.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
